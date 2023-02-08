ed83e6ade198e7cd15cbd1db22dfdfaf55b8ad08
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-08 15:40:42 +0100

    Dobbelsteen

--- les01Syntax/Dobbelstenen/js/node.js
+++ les01Syntax/Dobbelstenen/js/node.js
@@ -1,35 +1,27 @@
-function main(){
+
+const players = ['Magnus', 'Eline', 'Ding', 'Judith', 'Praggna'];
+const scores = [players.length];
+const NUM_DICE = 3;
 
 
-    const players = ['Magnus', 'Eline', 'Ding', 'Judith', 'Praggna'];
-    const scores = [players.length];
-    const NUM_DICE = 3;
-
-
-    console.log(`
+console.log(`
 DOBBELSTENEN
 ============
 
 
-aantal deelnemers: ${players.length}
-    `)
-    
-    
-    
-
-    for(let i = 0; i < players.length; i++){
-        console.log(`${players[i]} gooit: `);
-        let dice = [NUM_DICE];
-        for(let j = 0; j < NUM_DICE; j++){
-            dice[j] = Math.floor(Math.random()*7)+1;
-        }
-        scores[i] = GetTotal(dice);
-        console.log(`${dice.toString(dice)} (${scores[i]} punten)`);
+aantal deelnemers: ${players.length}`)
+for(let i = 0; i < players.length; i++){
+    console.log(`${players[i]} gooit: `);
+    let dice = [NUM_DICE];
+    for(let j = 0; j < NUM_DICE; j++){
+        dice[j] = Math.floor(Math.random()*7)+1;
+    }
+    scores[i] = GetTotal(dice);
+    console.log(`${dice.toString(dice)} (${scores[i]} punten)`);
     }
 
-    console.log(`\nWINNAAR:`);
-    console.log("%c" + GetWinner(scores, players), "background: yellow; color: black; font-size: 25px;");
-}
+console.log(`\nWINNAAR:`);
+console.log("%c" + GetWinner(scores, players), "background: yellow; color: black; font-size: 25px;");
 
 
 function DiceToString(dice){
---
+++ les01Syntax/JokerTrekking/js/node.js
@@ -0,0 +1,16 @@
+console.log(`
+//trekking 
+    
+getrokken getal: ${getal}`)
+console.log(`
+//gokken 
+    
+aantal iteraties: ${iteraties}`)
+console.log(`
+//resultaten
+    
+0 juist: ${getal[i]}
+1 juist: ${getal[i]}
+2 juist: ${getal[i]}
+3 juist: ${getal[i]}
+4 juist: ${getal[i]}`)
