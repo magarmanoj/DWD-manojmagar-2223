c6bf861db71d49783e5d8277fea255dccf07fc78
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-09 19:24:24 +0100

    joker

--- les01Syntax/JokerTrekking/js/node.js
+++ les01Syntax/JokerTrekking/js/node.js
@@ -2,50 +2,73 @@ const min = 1000;
 const max = 9999;
 const totaalSpeler = 10000;
 
-// const winst = [0, 2.5, 10, 100, 500];
-let trekken = getalGenereren();
-let play = getalGenereren()
+const winst = [0, 2.5, 10, 100, 500];
 
-console.log(`
+const trekken = getalGenereren();
+const resultaten = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
+};
+
+for (let i = 0; i < totaalSpeler; i++) {
+  const speler = getalGenereren();
+  const gelijk = matchingCijfer(trekken, speler);
+  resultaten[gelijk]++;
+}
+
+
+console.log(`%c
 //trekking 
     
-getrokken getal: ${trekken}`);
-console.log(`
+getrokken getal: ${trekken}`, 'font-size: 15px;');
+console.log(`%c
 //gokken 
     
-aantal iteraties: ${totaalSpeler}`);
-console.log(`
+aantal iteraties: ${totaalSpeler}`, 'font-size: 15px;');
+console.log(`%c
 //resultaten
     
-0 juist: 
-1 juist: 
-2 juist: 
-3 juist:
-4 juist: 
+0 juist: ${resultaten[0]}
+1 juist: ${resultaten[1]}
+2 juist: ${resultaten[2]}
+3 juist: ${resultaten[3]}
+4 juist: ${resultaten[4]}
+`, 'font-size: 15px;');
 
-gemiddelde winst: ${gemiddeldeWinst()}`);
-
-for (let i = 0; i < totaalSpeler; i++) { /* empty */ }
-
-
-
+console.log(`%cgemiddelde winst:  € ${gemiddeldeWinst().toFixed(3)}`, 'padding: 15px; background: grey; color: yellow; font-size: 20px;');
 
 function getalGenereren() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function matchingCijfer(getrokken, speler) {
-    getrokken = trekken.toString();
-    speler = play.toString();
-    let matched = 0;
-    for (let i = 0; i < speler; i++) {
-        if (trekken[i] == play[i]) {
-            matched++;
-        }
-    } return matched;
+    /* const getrokkenStr = getrokken.toString();
+    const spelerStr = speler.toString();*/
+
+    for (let i = 0; i < getrokken; i++) {
+      if (speler[i] % 10 === getrokken[i] % 10) {
+        resultaten[1]++;
+      }
+      if (speler[i] % 100 == getrokken[i] % 100) {
+        resultaten[2]++;
+      }
+      if (speler[i] % 1000 == getrokken[i] % 1000) {
+        resultaten[3]++;
+      } else {
+        resultaten[0]++;
+      }
+    }
+    return resultaten;
 }
 
 
-function gemiddeldeWinst(){
-
+function gemiddeldeWinst() {
+  let totaalWinst = 0;
+  for (let i = 0; i < 5; i++) {
+    totaalWinst += resultaten[i] * winst[i];
+  }
+  return totaalWinst / totaalSpeler;
 }
