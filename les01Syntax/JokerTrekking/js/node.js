cb9b3d5342625b9c06cb521af210a5682b2123c1
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-10 12:21:33 +0100

    jokertrekking

--- les01Syntax/JokerTrekking/js/node.js
+++ les01Syntax/JokerTrekking/js/node.js
@@ -5,13 +5,8 @@ const totaalSpeler = 10000;
 const winst = [0, 2.5, 10, 100, 500];
 
 const trekken = getalGenereren();
-const resultaten = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0
-};
+
+const resultaten = [0, 0, 0, 0, 0];
 
 for (let i = 0; i < totaalSpeler; i++) {
   const speler = getalGenereren();
@@ -19,19 +14,14 @@ for (let i = 0; i < totaalSpeler; i++) {
   resultaten[count]++;
 }
 
+console.log('%c//trekking', 'font-size: 20px; color: purple');
+console.log(`%c getrokken getal: ${trekken}`, 'font-size: 15px; color: yellow;');
 
-console.log(`%c
-//trekking 
-    
-getrokken getal: ${trekken}`, 'font-size: 15px;');
-console.log(`%c
-//gokken 
-    
-aantal iteraties: ${totaalSpeler}`, 'font-size: 15px;');
-console.log(`%c
-//resultaten
-    
-0 juist: ${resultaten[0]}
+console.log('%c\n//gokken', 'font-size: 20px; color: purple');
+console.log(`%c aantal iteraties: ${totaalSpeler}`, 'font-size: 15px;');
+
+console.log('%c\n//resultaten', 'font-size: 20px; color: purple');
+console.log(`%c0 juist: ${resultaten[0]}
 1 juist: ${resultaten[1]}
 2 juist: ${resultaten[2]}
 3 juist: ${resultaten[3]}
@@ -44,33 +34,25 @@ function getalGenereren() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function matchingCijfer(getrokken, speler) {
-    /* const getrokkenStr = getrokken.toString();
-    const spelerStr = speler.toString();*/
-    
+function matchingCijfer(getrokken, speler) {   
     const geTrokken = getrokken;
     const spelers = speler;
 
     let count = 0;
-    let i = 0;
-    while (i < geTrokken) {
-      if ((geTrokken % 10 == spelers % 10)) {
-        count++;
-        if ((geTrokken % 100 == spelers % 100)) {
-          count++;
-        }
-        if ((geTrokken % 1000 == spelers % 1000)) {
-          count++;
-        }
-        if ((geTrokken % (10 * 100) == spelers % (10 * 100))) {
-          count++;
-        }
-      }
-      i++;
-    } return count;
+    if ((geTrokken % 10 == spelers % 10)) {
+      count++;
+    }
+    if ((geTrokken % 100 == spelers % 100)) {
+      count++;
+    }
+    if ((geTrokken % 1000 == spelers % 1000)) {
+      count++;
+    }
+    if ((geTrokken % 10000 == spelers % 10000)) {
+      count++;
+    }
+    return count;
 }
-
-
 
 
 function gemiddeldeWinst() {
