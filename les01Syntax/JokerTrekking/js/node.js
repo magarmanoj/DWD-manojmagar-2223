5c57f49730b50484fc105f115891ac4d6d7eea4c
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-13 17:11:59 +0100

    Update node.js

--- les01Syntax/JokerTrekking/js/node.js
+++ les01Syntax/JokerTrekking/js/node.js
@@ -14,13 +14,13 @@ for (let i = 0; i < totaalSpeler; i++) {
   resultaten[count]++;
 }
 
-console.log('%c//trekking', 'font-size: 20px; color: purple');
+console.log('%c// trekking', 'font-size: 20px; color: purple');
 console.log(`%c getrokken getal: ${trekken}`, 'font-size: 15px; color: yellow;');
 
-console.log('%c\n//gokken', 'font-size: 20px; color: purple');
+console.log('%c\n// gokken', 'font-size: 20px; color: purple');
 console.log(`%c aantal iteraties: ${totaalSpeler}`, 'font-size: 15px;');
 
-console.log('%c\n//resultaten', 'font-size: 20px; color: purple');
+console.log('%c\n// resultaten', 'font-size: 20px; color: purple');
 console.log(`%c0 juist: ${resultaten[0]}
 1 juist: ${resultaten[1]}
 2 juist: ${resultaten[2]}
@@ -28,7 +28,7 @@ console.log(`%c0 juist: ${resultaten[0]}
 4 juist: ${resultaten[4]}
 `, 'font-size: 15px;');
 
-console.log(`%cgemiddelde winst:  € ${gemiddeldeWinst().toFixed(3)}`, 'padding: 15px; background: grey; color: yellow; font-size: 20px;');
+console.log(`%cgemiddelde winst:  €${gemiddeldeWinst().toFixed(3)}`, 'padding: 15px; background: grey; color: #bda800; font-size: 20px;');
 
 function getalGenereren() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
