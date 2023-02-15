6f37f3f8373e371cba219a053877372d8c1b8bd0
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-15 14:05:54 +0100

    pure methode en layout fout

--- les01Syntax/JokerTrekking/js/node.js
+++ les01Syntax/JokerTrekking/js/node.js
@@ -2,14 +2,15 @@ const min = 1000;
 const max = 9999;
 const totaalSpeler = 10000;
 
+// eslint-disable-next-line no-magic-numbers
 const winst = [0, 2.5, 10, 100, 500];
 
-const trekken = getalGenereren();
+const trekken = getalGenereren(min, max);
 
 const resultaten = [0, 0, 0, 0, 0];
 
 for (let i = 0; i < totaalSpeler; i++) {
-  const speler = getalGenereren();
+  const speler = getalGenereren(min, max);
   const count = matchingCijfer(trekken, speler);
   resultaten[count]++;
 }
@@ -27,36 +28,36 @@ console.log(`%c0 juist: ${resultaten[0]}
 3 juist: ${resultaten[3]}
 4 juist: ${resultaten[4]}`, 'font-size: 15px;');
 
-console.log(`%cgemiddelde winst:  €${gemiddeldeWinst().toFixed(3)}`, 'padding: 15px; background: grey; color: #bda800; font-size: 20px;');
+console.log(`%cgemiddelde winst:  €${gemiddeldeWinst(resultaten, totaalSpeler, winst).toFixed(3)}`, 'padding: 15px; background: grey; color: #bda800; font-size: 20px;');
 
-function getalGenereren() {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getalGenereren(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + max;
 }
 
-function matchingCijfer(getrokken, speler) {   
-    const eenheid = 10;
-    const hunderdtal = 100;
-    const duizendtal = 1000;
-    const tienDuizenTal = 10000;
+function matchingCijfer(getrokken, speler) {
+  const eenheid = 10;
+  const hunderdtal = 100;
+  const duizendtal = 1000;
+  const tienDuizenTal = 10000;
 
-    let count = 0;
-    if ((getrokken % eenheid == speler % eenheid)) {
-      count++;
-    }
-    if ((getrokken % hunderdtal == speler % hunderdtal)) {
-      count++;
-    }
-    if ((getrokken % duizendtal == speler % duizendtal)) {
-      count++;
-    }
-    if ((getrokken % tienDuizenTal == speler % tienDuizenTal)) {
-      count++;
-    }
-    return count;
+  let count = 0;
+  if ((getrokken % eenheid == speler % eenheid)) {
+    count++;
+  }
+  if ((getrokken % hunderdtal == speler % hunderdtal)) {
+    count++;
+  }
+  if ((getrokken % duizendtal == speler % duizendtal)) {
+    count++;
+  }
+  if ((getrokken % tienDuizenTal == speler % tienDuizenTal)) {
+    count++;
+  }
+  return count;   
 }
 
 
-function gemiddeldeWinst() {
+function gemiddeldeWinst(resultaten, totaalSpeler, winst) {
   let totaalWinst = 0;
   for (let i = 0; i < 5; i++) {
     totaalWinst += resultaten[i] * winst[i];
