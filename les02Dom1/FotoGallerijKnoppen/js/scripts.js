6a86b91ad09268cbd8ba55432cd29252ed72d5a3
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-17 15:57:30 +0100

    gallery

--- les02Dom1/FotoGallerijKnoppen/js/scripts.js
+++ les02Dom1/FotoGallerijKnoppen/js/scripts.js
@@ -1,11 +1,41 @@
 const figBig = document.querySelector('#figBig');
 const thumbLinks = document.querySelectorAll('.thumbs a');
+const photoIndex = [];
+const next = document.querySelectorAll('#next');
+const prev = document.querySelectorAll('#prev');
 
-thumbLinks.forEach(lnk => {
+thumbLinks.forEach((lnk, index) => {
     lnk.addEventListener('click', handleLinkClicks);
+    photoIndex.push(index);
+    console.log(photoIndex);
 });
 function handleLinkClicks(e) {
-   e.preventDefault();
-   figBig.querySelector('img').src = this.href;
-   figBig.querySelector('figcaption').innerHTML = this.querySelector('img').alt;
+    e.preventDefault();
+    figBig.querySelector('img').src = this.href;
+    figBig.querySelector('figcaption').innerHTML = this.querySelector('img').alt;
 }
+
+next.forEach((button, index) => {
+    button.addEventListener('click', function () {
+        photoIndex.push(index);
+        console.log(photoIndex);
+
+        const next = photoIndex[index]++;
+        const nextThumb = thumbLinks[next];
+        figBig.querySelector('img').src = nextThumb.href;
+        figBig.querySelector('figcaption').innerHTML = nextThumb.querySelector('img').alt;
+    });
+});
+
+
+prev.forEach((button, index) => {
+    button.addEventListener('click', function() {
+        photoIndex.push(index);
+        console.log(photoIndex);
+
+        const next = photoIndex[index]--;
+        const nextThumb = thumbLinks[next];
+        figBig.querySelector('img').src = nextThumb.href;
+        figBig.querySelector('figcaption').innerHTML = nextThumb.querySelector('img').alt;
+    });
+});
