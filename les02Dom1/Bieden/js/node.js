1f1f7f4f9d6ff398aeb5789b6f4210845660f2cb
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-18 01:42:47 +0100

    bieden

---
+++ les02Dom1/Bieden/js/node.js
@@ -0,0 +1,23 @@
+const inpNaam = document.querySelector('#naam');
+const inpBod = document.querySelector('#bod');
+const btn = document.querySelector('button');
+const msg = document.querySelector('#lblMsg');
+let hoogsteBod = 0;
+let hoogsteBidder = ' ';
+
+btn.addEventListener('click', function() {
+    const huidigBod = inpBod.value;
+    const huidigBidder = inpNaam.value;
+    if (huidigBod > hoogsteBod) {
+        hoogsteBod = huidigBod;
+        hoogsteBidder = huidigBidder;
+        msg.innerHTML = `gefeliciteerd! je hebt momenteel het hoogste bod met een bod van €${hoogsteBod}!`;
+    } 
+    if (huidigBod == '' || hoogsteBidder == '') {
+        msg.innerHTML = 'er is nog geen bod uitgebracht';
+    }
+    if (huidigBod < hoogsteBod) {  
+        msg.innerHTML = `jammer! ${hoogsteBidder} heeft een hoger bod`;
+    } 
+});
+
