154760e21336887ba529fe17ccad9903fd4b449f
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-03-09 22:26:25 +0100

    pralines

--- les03Dom2/Pralines/js/script.js
+++ les03Dom2/Pralines/js/script.js
@@ -1,5 +1,5 @@
 const DOM = {
-    photos: document.querySelectorAll('.soortPralines li a img'),
+    photos: document.querySelectorAll('.soortPralines li img'),
     naam: document.querySelector('#naam'),
     calorieën: document.querySelector('#calorieën'),
     beschrijving: document.querySelector('#beschrijving'),
@@ -9,11 +9,12 @@ const DOM = {
 DOM.photos.forEach(photo => {
     photo.addEventListener('click', function(e) {
         e.preventDefault();
-        DOM.photos.forEach(active => active.classList.remove('active'));
+        document.querySelector('img.active').classList.remove('active');
         photo.classList.add('active');
+
         DOM.naam.innerHTML = photo.getAttribute('alt');
-        DOM.calorieën.innerHTML = photo.parentNode.parentNode.getAttribute('data-calorieën');
-        DOM.beschrijving.innerHTML = photo.parentNode.parentNode.getAttribute('data-beschrijving');
+        DOM.calorieën.innerHTML = photo.parentNode.getAttribute('data-calorieën');
+        DOM.beschrijving.innerHTML = photo.parentNode.getAttribute('data-beschrijving');
     });    
 });
 
