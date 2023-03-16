2026f6e973d8993bdcbea5ae6ff35868bc674bbc
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-03-16 16:45:11 +0100

    Norris

--- les04WebApiAjax/ChuckNorrisQuotes/js/scripts.js
+++ les04WebApiAjax/ChuckNorrisQuotes/js/scripts.js
@@ -1,17 +1,38 @@
 const DOM = {
-    quotes: document.querySelector('.quote')
+    quotes: document.querySelector('.quote'),
+    categories: document.querySelector('#category')
 };
 
-async function giveQuote() {
+DOM.categories.addEventListener('change', () => {
+    const category = DOM.categories.value;
+    if (category) {
+        giveQuote(category);
+    }
+    else {
+        giveQuoteZonderCat();
+    }
+});
+
+async function giveQuote(category = '') {
+    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+    const resp = await fetch(url);
+    if (!resp.ok) {
+        console.log('mislukt');
+        return;
+    }
+    const data = await resp.json();
+    DOM.quotes.innerHTML = `${data.value}<cite>Chuck Norris</cite>`;
+}
+
+async function giveQuoteZonderCat() {
     const url = 'https://api.chucknorris.io/jokes/random';
     const resp = await fetch(url);
     if (!resp.ok) {
         console.log('mislukt');
         return;
     }
-
     const data = await resp.json();
-    DOM.quotes.innerHTML = data.value;
+    DOM.quotes.innerHTML = `${data.value}<cite>Chuck Norris</cite>`;
 }
 
-giveQuote();
+giveQuoteZonderCat();
