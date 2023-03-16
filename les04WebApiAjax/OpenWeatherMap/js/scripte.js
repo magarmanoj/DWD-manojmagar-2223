94444d97a9b42cdaa8f417d0df6b3d7f81cdcd0c
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-03-16 18:04:47 +0100

    Weather

---
+++ les04WebApiAjax/OpenWeatherMap/js/scripte.js
@@ -0,0 +1,24 @@
+const DOM = {
+    message: document.querySelector('#message')
+};
+
+async function getWeather() {
+    const apiKey = '8ec419b896e22640ad472aa5b0dd60cc';
+    const lat = '50.8476';
+    const lon = '4.3572';
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=nl&appid=${apiKey}`;
+
+    const resp = await fetch(url);
+
+    if (!resp.ok) {
+        console.log('mislukt');
+        return;
+    }
+
+    const data = await resp.json();
+    const description = data.weather[0].description;
+
+    DOM.message.innerHTML = `Het is in ${data.name} ${data.main.temp}°C en ${description} .`;
+}
+
+getWeather();
