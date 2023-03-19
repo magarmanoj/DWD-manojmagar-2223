d90c79f2760eb88ab7d0ade0f8b79fb66624237d
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-03-19 02:58:22 +0100

    project html en css gebrobeert

---
+++ project/jss/scripts.js
@@ -0,0 +1,8 @@
+// const bgcolor = `rgb(${+Math.random() * 255}, ${+Math.random() * 255}, ${+Math.random() * 255})`;
+// document.querySelector('#demobutton .sound__background').style.backgroundColor = bgcolor;
+
+const buttons = document.querySelectorAll('.sound__background');
+buttons.forEach((button) => {
+  const bgcolor = `rgb(${+Math.random() * 255}, ${+Math.random() * 255}, ${+Math.random() * 255})`;
+  button.style.backgroundColor = bgcolor;
+});
