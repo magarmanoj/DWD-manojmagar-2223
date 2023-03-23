5f9fb1911e7d50a1203d8ba27de66f1a770821c0
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-03-23 03:59:01 +0100

    try

--- project/jss/scripts.js
+++ project/jss/scripts.js
@@ -1,24 +1,29 @@
-// const bgcolor = `rgb(${+Math.random() * 255}, ${+Math.random() * 255}, ${+Math.random() * 255})`;
-// document.querySelector('#demobutton .sound__background').style.backgroundColor = bgcolor;
+const bgcolor = `rgb(${+Math.random() * 255}, ${+Math.random() * 255}, ${+Math.random() * 255})`;
+document.querySelector('#demobutton .sound__background').style.backgroundColor = bgcolor;
 
-const buttons = document.querySelectorAll('.sound__background');
-buttons.forEach((button) => {
-  // eslint-disable-next-line no-magic-numbers
-  const bgcolor = `rgb(${+Math.random() * 255}, ${+Math.random() * 255}, ${+Math.random() * 255})`;
-  button.style.backgroundColor = bgcolor;
-});
 
-const DOM = {
-  stats: document.querySelector('.stats')
-};
+// const buttons = document.querySelectorAll('.sound__background');
+// buttons.forEach((button) => {
+//   const bgcolor = `rgb(${+Math.random() * 255}, ${+Math.random() * 255}, ${+Math.random() * 255})`;
+//   button.style.backgroundColor = bgcolor;
+// });
 
-async function getStatus() {
-  const apiKey = '2NyW7omHomOYDbyvmxizDsTZxSRLdgxH1JscuTKD';
-  const url = `https://freesound.org/apiv2/search/text/?query=piano&token=${apiKey}`;
-  const resp = await fetch(url);
+// const apiKey = '2NyW7omHomOYDbyvmxizDsTZxSRLdgxH1JscuTKD';
+// const search = 'piano';
+// const url = `https://freesound.org/apiv2/search/text/?query=${search}&token=${apiKey}`;
 
-  if (!resp.ok) return console.log('mislukt');
-  const data = await resp.json();
-  DOM.stats.innerHTML = `${data.value}`;
-}
-getStatus();
+
+// const DOM = {
+//   canvas: document.querySelector('#sound-waveform'),
+//   times: document.querySelector('#sound-time')
+// };
+
+// async function getStatus() {
+//   const resp = await fetch(url);
+
+//   if (!resp.ok) return console.log('mislukt');
+//   const data = await resp.json();
+//   DOM.canvas.innerHTML = data.value;
+//   console.log(data);
+// }
+// getStatus();
